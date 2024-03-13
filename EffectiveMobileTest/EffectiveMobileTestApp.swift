0958ff1d2cf39f4e857d488513967630e8ffec06
alex <alex.vasilevski007@gmail.com>
2024-03-13 16:15:12 +0300

    Initial Commit

---
+++ EffectiveMobileTest/EffectiveMobileTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  EffectiveMobileTestApp.swift
+//  EffectiveMobileTest
+//
+//  Created by Alex on 13.03.24.
+//
+
+import SwiftUI
+
+@main
+struct EffectiveMobileTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
